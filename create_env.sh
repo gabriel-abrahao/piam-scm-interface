a088b24f08f5b3e59a729c6a01f64ba1d6492d88
gabriel-abrahao <gabrielmabrahao@gmail.com>
2021-07-30 04:20:46 +0200

    Force remove an existing venv when creating a new one

--- create_env.sh
+++ create_env.sh
@@ -2,6 +2,7 @@
 # This creates an environment using venv
 # If you want to use it interactively you need to activate it again
 newenvname='venv_scm' # Hardcoded here
+rm -rf $newenvname
 python3 -m venv $newenvname --clear
 source $newenvname'/bin/activate'
 python3 -m pip install -r requirements.txt
